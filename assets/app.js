bf1385b52721a00bd2a236f751518fa2d141a45b
bparvin12 <bparvin12@gmail.com>
2018-11-29 11:31:55 -0800

    working

---
+++ assets/app.js
@@ -0,0 +1,46 @@
+function startSearch () {
+
+var apiKey = "cf39c6a130964054954684fac7e98dc1"
+var search = $('#search').val();
+var beginDate = $("#startYear").val();
+var endDate = $("#endYear").val();
+
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+queryURL += '?' + $.param({
+  'api-key': apiKey,
+  'q': search,
+//   'begin_date': beginDate,
+//   'end_date': endDate,
+});
+$.ajax({
+  url: queryURL,
+  method: 'GET',
+}).then(function(response) {
+console.log(response);
+
+var results = response.response.docs;
+
+// var numberOfArticles = $(".class").attr("value")
+
+for (var i = 0; i < 8; i++) {
+    var totalArticle = $("<div>")
+
+    var articles = $("<div>");
+
+    var title = $("<h3>").text(results[i].headline.main)
+
+    articles.append(results[i].web_url);
+
+    totalArticle.append(title)
+    totalArticle.append(articles)
+
+    $("#articles").append(totalArticle);
+} 
+});
+
+};
+
+
+
+$(document).on('click', '#searchButton', startSearch);
